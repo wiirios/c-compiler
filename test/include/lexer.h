4d1203f0e61cf1eb19d9430c04d8fa6609c1636d
wiirios <recuperacaowilliamrios@gmail.com>
2026-08-28 21:09:11 -0300

    test(feat): implement tokens struct, new gotos statement and update headers

--- test/include/lexer.h
+++ test/include/lexer.h
@@ -1,7 +1,7 @@
 #ifndef LEXER_HEADER
 #define LEXER_HEADER
 
-#include "../include/utils.h"
+#include "token.h"
 
 enum PRIMITIVE_DATA_TYPE_C_ENUM {
 	INTEGER_PRIMITIVE_DATA_TYPE,
@@ -10,7 +10,7 @@ enum PRIMITIVE_DATA_TYPE_C_ENUM {
 	LONG_PRIMITIVE_DATA_TYPE
 };
 
-void scan_t(char *phrase, int *block);
+void scan_t(char *phrase, int *block, tokens_t *tokens);
 
 int get_phrase_type(char *buffer);
 
--- test/src/lexer.c
+++ test/src/lexer.c
@@ -1,4 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
 #include "../include/lexer.h"
+#include "../include/parser.h"
+#include "../include/reader.h"
+#include "../include/utils.h"
 
 char *PRIMITIVE_DATA_TYPES_C[] = {
 	"int",
@@ -14,22 +22,36 @@ int TYPE_C[] = {
 	LONG_ENUM,
 };
 
-void scan_t(char *phrase, int *block) {
+void scan_t(char *phrase, int *block, tokens_t *tokens) {
 	token_t *token;
-	int value, i;
+	int value, i;	
 	extern int *ptr_phrase_type;
 	char BUFFER[strlen(phrase) + 1];
 	
+	//if (!tokens) tokens = init_tokens_t();
+	//if(*phrase != ' ' && *phrase != '\n' && *phrase != '\0') tokens->length+=1;
+	
 	value = i = 0;
-
+		
 	if (isdigit(*phrase)) goto is_num;
 	
 	if (isalpha(*phrase) && (!IS_OPERATOR(*phrase) || !IS_PUNCTUATORS(*phrase))) goto is_char;
 	
-	if (IS_OPERATOR(*phrase) || IS_PUNCTUATORS(*phrase)) goto not_num;
+	if (IS_OPERATOR(*phrase) || IS_PUNCTUATORS(*phrase) || IS_ASSIGNMENT(*phrase) || IS_LOGICAL(*phrase) || IS_SEMICOLON(*phrase) || IS_ASSIGNMENT(*phrase)) goto is_signal;
 	
 	return;
 	
+	is_assignment:
+		memcpy(BUFFER, phrase, strlen(phrase) + 1);
+		token = init_token_t("ASSIGNMENT", &BUFFER, block, ASSIGMENT_ENUM);
+		
+		memset(BUFFER, 0, strlen(BUFFER));
+		print_token_t(token);
+		free_token_t(token);
+		tokens->length+=1;
+		
+		return;
+		
 	is_num:
 		while (*phrase) {
 			if (!isdigit(*phrase)) {
@@ -40,10 +62,12 @@ void scan_t(char *phrase, int *block) {
 			value = 10 * value + (*phrase - '0');
 			phrase++;
 		}
-			
+		
 		token = init_token_t("INT", &value, block, INTEGER_ENUM);
+		
 		print_token_t(token);
-		free_token_t(token);
+		tokens->array_tokens[tokens->length] = token;
+		tokens->length+=1;		
 		
 		return;
 		
@@ -55,24 +79,46 @@ void scan_t(char *phrase, int *block) {
 			//return;
 		//}
 		memcpy(BUFFER, phrase, strlen(phrase) + 1);
-				
+			
+		if (IS_RESERVED_WORD(BUFFER)) {
+			int i = 0;
+			
+			for (; i < strlen(BUFFER); i++) BUFFER[i] = toupper((unsigned char)BUFFER[i]);			
+			
+			token = init_token_t(BUFFER, &BUFFER, block, RESERVED_ENUM);
+			memset(BUFFER, 0, strlen(BUFFER));
+			
+			tokens->array_tokens[tokens->length] = token;		
+			print_token_t(token);
+			
+			tokens->length+=1;
+			return;
+		}
+			
 		token = init_token_t("CHAR", &BUFFER, block, CHAR_ENUM);
 		memset(BUFFER, 0, strlen(BUFFER));
+		
 		print_token_t(token);
-		free_token_t(token);
-				
+		tokens->array_tokens[tokens->length] = token;
+		
+		tokens->length+=1;
 		return;
 		
-	not_num:
-		if (__clang__) {
-			;
-		}
-		int signal = *phrase;
+	is_signal:
+		memcpy(BUFFER, phrase, strlen(phrase) + 1);
 		
-		token = init_token_t(IS_OPERATOR(*phrase) ? "OPERATOR": "PUNCTUATORS", &signal, block, IS_OPERATOR(*phrase) ? OPERATOR_ENUM: PUNCTUATORS_ENUM);
+		if (IS_OPERATOR(*phrase)) token = init_token_t("OPERATOR", &BUFFER, block, OPERATOR_ENUM);
+		else if (IS_PUNCTUATORS(*phrase)) token = init_token_t("PUNCTUATORS", &BUFFER, block, PUNCTUATORS_ENUM);
+		else if (IS_ASSIGNMENT(*phrase)) token = init_token_t("ASSIGNMENT", &BUFFER, block, ASSIGMENT_ENUM);
+		else if (IS_LOGICAL(*phrase)) token = init_token_t("LOGICAL", &BUFFER, block, LOGICAL_ENUM);
+		else if (IS_SEMICOLON(*phrase)) token = init_token_t("SEMICOLON", &BUFFER, block, SEMICOLON_ENUM);
+		else if (IS_ASSIGNMENT(*phrase)) token = init_token_t("ASSIGNMENT", &BUFFER, block, ASSIGMENT_ENUM);
+		
+		memset(BUFFER, 0, strlen(BUFFER));
 		print_token_t(token);
-		free_token_t(token);
+		tokens->array_tokens[tokens->length] = token;
 		
+		tokens->length+=1;
 		return;
 }
 
