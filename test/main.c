14612f8ab1bcf27c0bedf86069d75a480c94a8b4
wiirios <recuperacaowilliamrios@gmail.com>
2026-08-28 21:10:33 -0300

    test(feat): update main with new tokens struct logic

--- test/main.c
+++ test/main.c
@@ -1,50 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include "include/token.h"
 #include "include/utils.h"
+#include "include/reader.h"
+#include "include/hashtable.h"
+#include "include/lexer.h"
+#include "include/parser.h"
 
-#define TEST 1
+#define DEBUG 1
 
 /*
  *
- *
+ *	
 */
 static int block = 0;
 static int phrase_type = -1;
+static int assignment = 0;
+
 int *ptr_phrase_type = &phrase_type;
 
-int main(int argc, char *argv[]) {
-	#if TEST
-	
-	ht_t *ht = ht_init();
-	
-	ht_printall(ht);
-	
-	get(ht, "float");
-	get(ht, "char");
-	
-	hte_destroy(ht);
-	ht_destroy(ht);
-	
-	printf("exiting...\n");	
-	exit(EXIT_SUCCESS);
-	#endif
-	
-	
+int main(int argc, char *argv[]) {	
 	if (argc < 2) error_t("argv < 2");
 	
 	FILE *file = get_file_t(argv[1]);
 	
 	char BUFFER_[BUFFER_SIZE_T];
+	ht_t *ht = ht_init();
+	
+	tokens_t *tokens = init_tokens_t();
 	
 	while (feof(file) == 0) {	
-		reader_file_t(BUFFER_, sizeof(BUFFER_), file, &block);	
-		scan_t(BUFFER_, &block);
+		reader_file_t(BUFFER_, sizeof(BUFFER_), file, &block);
+		scan_t(BUFFER_, &block, tokens);
+		
+		if (IS_SEMICOLON(BUFFER_[strlen(BUFFER_) - 1])) {
+			if (get_grammar(tokens)) printf("valid grammar\n");
+			else printf("invalid grammar\n");
+		}
 		
 		memset(BUFFER_, 0, strlen(BUFFER_));
 	}
 	
-	printf("\nnumber os blocks: %d\n", block);
-	
+	print_token_t_parser(tokens);
+	free_tokens_t(tokens);
+	free(tokens);
+	ht_destroy(ht);
 	fclose(file);
+	
+	#if TEST
+	printf("\nnumber os blocks: %d", block);
+	printf("\ntotal tokens read: %d\n", tokens->length);
+	
+	free(tokens);
+	#else
+	printf("\n");
+	#endif	
+
 	return 0;
 }
