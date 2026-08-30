d5bb7b8a83fe5f9e44e6b1cacf65248a513fd932
wiirios <recuperacaowilliamrios@gmail.com>
2026-08-29 23:13:50 -0300

    test(feat):implements a linked list in the syntactic analysis and its specific basic rules

--- test/include/parser.h
+++ test/include/parser.h
@@ -2,10 +2,58 @@
 #define PARSER_H
 
 #include "token.h"
+#include "reader.h"
+
+#define DECLARATION_SIZE 5
 
 void print_token_t_parser(tokens_t *tokens);
 
-int declartion(tokens_t *tokens);
-int get_grammar(tokens_t *tokens);
+int parser(tokens_t *tokens);
+
+int type(token_t *token);
+int id(token_t *token);
+int assigment(token_t *token);
+int expression(token_t *token);
+int semicolon(token_t *token);
+
+typedef struct node node_t;
+typedef struct ll ll_t;
+
+struct ll {
+	node_t *head;
+};
+
+struct node {
+	token_t *token;
+	node_t *next;
+};
+
+ll_t *init_ll(void);
+node_t *init_node(token_t *token);
+
+int declaration(ll_t *ll, tokens_t *tokens);
+
+void insert_node(ll_t *ll, token_t *token);
+void free_ll(ll_t *ll);
+void print_ll(ll_t *ll);
 
 #endif
+
+/*
+ * declaration -> type id = expression ;
+ * type -> int | char | float | long
+ * id -> [Aa-Zz]+
+*/
+
+/*
+ * expression -> factor
+ * expression -> factor + factor
+ * 				| factor - factor
+ * 				| factor / factor
+ * 				| factor * factor
+ * 				| (expression)
+ * 				| expression 
+ * factor -> number+
+ * number -> 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 
+ */
+
--- test/src/parser.c
+++ test/src/parser.c
@@ -6,32 +6,132 @@
 #include "../include/parser.h"
 #include "../include/utils.h"
 
-/*
- * declaration -> type id = expresion ;
- * type -> int | char | float | long
- * id -> [Aa-Zz]+
-*/
-
-/*
- * expression -> factor
- * expression -> factor + factor
- * 				| factor - factor
- * 				| factor / factor
- * 				| factor * factor
- * 				| (expression)
- * 				| expression 
- * factor -> number+
- * number -> 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 
- */
+int parser(tokens_t *tokens) {
+	if (tokens->length < 5) return 0;
+	
+	ll_t *ll = init_ll();
+	
+	if (!declaration(ll, tokens)) {
+		free_ll(ll);
+		free_tokens_t(tokens);
+		error_t("invalid declaration");	
+	}
+	
+	free_ll(ll);
+	return 1;
+} 
 
 void print_token_t_parser(tokens_t *tokens) {
 	for (int i = 0; i < tokens->length; i++) if (tokens->array_tokens[i] != NULL) printf("token->identifier: %s at :[%d]\n", tokens->array_tokens[i]->identifier, i);
 }
 
-int declartion(tokens_t *tokens) {
-	return 0;
+int declaration(ll_t *ll, tokens_t *tokens) {
+	int (*alldecl[DECLARATION_SIZE])(token_t *token) = 
+	{	type, 
+		id, 
+		assigment, 
+		expression, 
+		semicolon
+	};
+	int i = 0;
+	
+	while (tokens->array_tokens[i] != NULL) {
+		if (!alldecl[i](tokens->array_tokens[i])) return 0;
+		
+		insert_node(ll, tokens->array_tokens[i]);
+		i++;
+	}
+	
+	return 1;
 }
 
-int get_grammar(tokens_t *tokens) {
+int type(token_t *token) {
+	if (token->type != RESERVED_ENUM) return 0;
+	
 	return 1;
+}
+
+int id(token_t *token) {
+	if (token->identifier == NULL || *token->identifier == '\0') return 0;
+	
+	return 1;
+}
+
+int assigment(token_t *token) {
+	if (token->type != ASSIGMENT_ENUM && !IS_ASSIGNMENT(*token->identifier) && strlen(token->identifier) != 1) return 0;
+	
+	return 1;
+}
+
+int expression(token_t *token) {
+	if (token->type != INTEGER_ENUM) return 0;
+	
+	return 1;
+}
+
+int semicolon(token_t *token) {
+	if (token->type != SEMICOLON_ENUM && !IS_SEMICOLON(*token->identifier) && strlen(token->identifier) != 1) return 0;
+	
+	return 1;
+}
+
+ll_t *init_ll(void) {
+	ll_t *ll = (ll_t*) malloc(sizeof(ll_t));
+
+	if (!ll) {
+		//
+		error_t("Error while creating linked list\n");
+	}
+	
+	ll->head = NULL;
+	
+	return ll;
+}
+
+node_t *init_node(token_t *token) {
+	node_t *node = (node_t*) malloc(sizeof(node_t));
+	
+	if (!node) {
+		error_t("\n");	
+	}
+	
+	node->next = NULL;
+	node->token = token;
+	
+	return node;
+}
+
+void insert_node(ll_t *ll, token_t *token) {
+	node_t *node = init_node(token);
+	
+	if (ll->head == NULL) {
+		ll->head = node;
+		
+		return;
+	}
+	
+	for (node_t *current = ll->head; current != NULL; current = current->next) {
+		if (current->next == NULL) {
+			current->next = node;
+			break;
+		}
+	}	
+}
+
+void free_ll(ll_t *ll) {
+	node_t *current;
+	
+	while ((current = ll->head) != NULL) {
+		ll->head = ll->head->next;
+		free(current);	
+	}
+}
+
+void print_ll(ll_t *ll) {
+	node_t *current;
+	
+	while ((current = ll->head) != NULL) {
+		printf("ll token->identifier: %s\n", current->token->identifier);	
+		ll->head = ll->head->next;
+	}
 }
