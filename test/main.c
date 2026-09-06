0e9e6e04c027e5e3e74c790ce3417a984a4979f6
wiirios <recuperacaowilliamrios@gmail.com>
2026-09-06 15:36:42 -0300

    test(refactor): improved debug informations with argc flag

--- test/main.c
+++ test/main.c
@@ -9,15 +9,10 @@
 #include "include/lexer.h"
 #include "include/parser.h"
 
-#define DEBUG 1
-
-/*
- *
- *	
-*/
 static int block = 0;
 static int phrase_type = -1;
 static int assignment = 0;
+ht_t *ht = NULL;
 
 int *ptr_phrase_type = &phrase_type;
 
@@ -27,7 +22,7 @@ int main(int argc, char *argv[]) {
 	FILE *file = get_file_t(argv[1]);
 	
 	char BUFFER_[BUFFER_SIZE_T];
-	ht_t *ht = ht_init();
+	ht = ht_init();
 	
 	tokens_t *tokens = init_tokens_t();
 	
@@ -35,28 +30,50 @@ int main(int argc, char *argv[]) {
 		reader_file_t(BUFFER_, sizeof(BUFFER_), file, &block);
 		scan_t(BUFFER_, &block, tokens);
 		
-		if (IS_SEMICOLON(BUFFER_[strlen(BUFFER_) - 1])) {
-			if (get_grammar(tokens)) printf("valid grammar\n");
-			else printf("invalid grammar\n");
+		if (IS_SEMICOLON(BUFFER_[strlen(BUFFER_) - 1]) || IS_PUNCTUATORS(BUFFER_[strlen(BUFFER_) - 1])) {
+			if (!parser(tokens)) printf("\ninvalid grammar\n");
+			else printf("\nvalid grammar\n");
+			
+			if (argc == 3) {
+				if (strcmp(argv[2], "-y") == 0) {
+					printf("\n--\n");
+					for (int i = 0; i < tokens->length; i++) {
+						if (tokens->array_tokens[i]) {
+							printf("array_tokens[%d] = ", i);
+							print_token_t(tokens->array_tokens[i]);
+							printf(" || type: %d" , tokens->array_tokens[i]->type);
+							printf(" || block: %d", tokens->array_tokens[i]->block);
+							printf("\n");
+						}
+					}
+					printf("tokens->length = %d\n", tokens->length);
+					printf("--\n");
+				}
+				else {
+					free_tokens_t(tokens);
+					free(tokens);
+					ht_destroy(ht);
+					fclose(file);
+					error_t("invalid argv at: argv[3]");
+				}
+			}
+			
+			// reset tokens	
+			free_tokens_t(tokens);
+			free(tokens);
+			tokens = init_tokens_t();
 		}
 		
 		memset(BUFFER_, 0, strlen(BUFFER_));
 	}
 	
-	print_token_t_parser(tokens);
-	free_tokens_t(tokens);
-	free(tokens);
+	if (tokens) {
+		free_tokens_t(tokens);
+		free(tokens);
+	}
+	
 	ht_destroy(ht);
 	fclose(file);
-	
-	#if TEST
-	printf("\nnumber os blocks: %d", block);
-	printf("\ntotal tokens read: %d\n", tokens->length);
-	
-	free(tokens);
-	#else
-	printf("\n");
-	#endif	
 
 	return 0;
 }
